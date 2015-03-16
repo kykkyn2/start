460b4287708f031f9cc1433ca9b19133c17d9e7a
kykkyn2 <kykkyn2@gmail.com>
2015-03-17 01:55:56 +0900

    작업준비

---
+++ start/src/main/java/com/overfunc/controller/ViewController.java
@@ -0,0 +1,18 @@
+package com.overfunc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ViewController {
+  
+    @RequestMapping(value = { "/index.htm", "/", "" }, method = RequestMethod.GET)
+    public String siteIndex(HttpServletRequest request) {
+        return "index";
+    }
+    
+    
+}
---
+++ start/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,12 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+	<title>LOVE</title>
+	<script src="http://code.jquery.com/jquery-1.8.2.js"></script>
+</head>
+<body>
+<h1>
+	It works!
+</h1>
+</body>
+</html>
